b8998a55108d7588a75dd858944511e94ac336b1
Regina Doi Pillon <regina123doipillon@gmail.com>
2026-06-07 13:59:08 -0300

    excluir itens da janela prinicpal

--- Janela.java
+++ Janela.java
@@ -9,6 +9,9 @@ public class Janela extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
 
+        JPanel painelLogin = new JPanel();
+        painelLogin.setLayout(null);
+
         JLabel endereco_login = new JLabel("Digite seu email/telefone:");
         endereco_login.setBounds(75, 50, 150, 15);
         JTextField login = new JTextField();
@@ -26,17 +29,21 @@ public class Janela extends JFrame{
         criar_conta.setBounds(100, 400, 100, 50);
 
         entrar.addActionListener(e->{
-           janela.getContentPane().removeAll();
+            getContentPane().remove(painelLogin);
+            revalidate(); // recalcula o layout
+            repaint();    // redesenha a tela
         });
         
         setLayout(null);
-        //add-> coloca os componentes dentro da Janela
-        add(endereco_login);
-        add(login);
-        add(senha_login);
-        add(senha);
-        add(entrar);
-        add(criar_conta);
+        //add-> coloca os componentes dentro do JPanel
+        painelLogin.add(endereco_login);
+        painelLogin.add(login);
+        painelLogin.add(senha_login);
+        painelLogin.add(senha);
+        painelLogin.add(entrar);
+        painelLogin.add(criar_conta);
+        setLayout(new BorderLayout());
+        add(painelLogin, BorderLayout.CENTER);
         setVisible(true);
     }
     public static void main(String[] args){
